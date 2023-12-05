25836c5324486e9ad3a97cfd85420625d9ebfa2b
Taleh Ibrahimli <talehsmail@gmail.com>
2023-12-05 21:11:30 +0400

    major fixes

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ git pull
 
 ## building nano
 mvn clean package
-docker build -f local.Dockerfile . -t apibrew/nano:latest --platform linux/amd64
+docker build -f local.Dockerfile . -t docker-registry.apibrew.io/nano:latest --platform linux/amd64
 cd ..
 ## end building backend
 
-docker push apibrew/nano:latest
+docker push docker-registry.apibrew.io/nano:latest
 
