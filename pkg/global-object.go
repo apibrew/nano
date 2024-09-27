777e07fc4088088b3fdd1d381b15f27f48c772d9
Taleh Ibrahimli <talehsmail@gmail.com>
2024-09-27 10:05:59 +0200

    fix race conditions problems on global object

--- pkg/global-object.go
+++ pkg/global-object.go
@@ -1,18 +1,27 @@
 package nano
 
+import "sync"
+
 type globalObject struct {
 	innerData map[string]interface{}
+	mu        sync.RWMutex
 }
 
 func (g *globalObject) Define(name string, value interface{}) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	g.innerData[name] = value
 }
 
 func (g *globalObject) UnDefine(name string) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	delete(g.innerData, name)
 }
 
 func (g *globalObject) Get(name string) interface{} {
+	g.mu.RLock()
+	defer g.mu.RUnlock()
 	return g.innerData[name]
 }
 
